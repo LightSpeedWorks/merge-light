fea237163b0494db2b54a403d9e042a86498f6fa
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-12-01 21:58:45 +0900

    space to tab

--- merge-light.js
+++ merge-light.js
@@ -1,18 +1,18 @@
 // merge-light.js
 
 void function () {
-  'use strict';
+	'use strict';
 
-  function merge(dst, src) {
-    for (var i = 1; src = arguments[i], i < arguments.length; ++i)
-      for (var p in src)
-        if (src.hasOwnProperty(p) && !dst.hasOwnProperty(p) &&
-            dst[p] !== src[p]) dst[p] = src[p];
-    return dst;
-  }
+	function merge(dst, src) {
+		for (var i = 1; src = arguments[i], i < arguments.length; ++i)
+			for (var p in src)
+				if (src.hasOwnProperty(p) && !dst.hasOwnProperty(p) &&
+						dst[p] !== src[p]) dst[p] = src[p];
+		return dst;
+	}
 
-  if (typeof module === 'object' && module && module.exports)
-    module.exports = merge;
+	if (typeof module === 'object' && module && module.exports)
+		module.exports = merge;
 
-  Function('return this')()['merge-light'] = merge;
+	Function('return this')()['merge-light'] = merge;
 }();
